06d84b93392ee12de23f32bb931b017d15a3d62b
Ghidyon <gideonakunana@gmail.com>
2020-08-28 23:09:38 +0100

    second commit

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 function multiply() {
-    let numberValue = Number(document.querySelector('#numberValue').value);
+    numberValue = Number(document.querySelector('#numberValue').value);
     console.log(numberValue);
     multiplierRange = Number(document.querySelector('#range').value);
     console.log(multiplierRange);
-    console.log(typeof numberValue);
-    console.log(typeof multiplierRange);
     sign = "\u00d7";
-    result = "";
+    multiplicationTable = document.querySelector('table');
 
-    for (i = 1; i <= multiplierRange; i++) {
-        result = numberValue * i;
-        console.log(result);
-        document.querySelector('.values').innerHTML = numberValue;
-        document.querySelector('.sign').innerHTML = sign;
-        document.querySelector('.ranges').innerHTML = i;
-        document.querySelector('.output').innerHTML = result;
+    if (multiplierRange <= 0) {
+        alert('Cannot calculate numbers below 1 in the range field.');
+        return false;
+    } else if (numberValue == 0) {
+        alert('Input any number asides 0 in the input number field');
+        return false;
+    } else {
+        for (i = 1; i <= multiplierRange; i++) {
+            result = numberValue * i;
+            tableRow = document.createElement('tr');
+            tableRow.innerHTML = `<td class="values" style="border: 2px solid orange;">${numberValue}</td>
+            <td class="sign" style="border: 2px solid red;">${sign}</td>
+            <td class="ranges" style="border: 2px solid green;">${i}</td>
+            <td class="output" style="border: 2px solid blue;">${result}</td>`;
+            document.querySelector('tbody').appendChild(tableRow);
+        }
     }
 }
