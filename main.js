ce582d16a73178164c4d34b154eea120779314be
Ghidyon <gideonakunana@gmail.com>
2020-08-29 01:30:19 +0100

    fifth commit

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-function multiply() {
-    //Define variables for  
-    numberValue = document.querySelector('#numberValue').value;
-    console.log(numberValue);
-    multiplierRange = document.querySelector('#range').value;
-    console.log(multiplierRange);
-    sign = "\u00d7";
-    multiplicationTable = document.querySelector('table');
-    inputNumber = Number(numberValue);
+//Define variables for conditions
+let numberValue = document.querySelector('#numberValue'),
+    multiplierRange = document.querySelector('#range'),
+    multiplicationTable = document.querySelector('table'),
+    generateBtn = document.querySelector('.btn'),
+    table = document.querySelector('table');
 
-    if (numberValue === "" && multiplierRange === "") {
+function multiply() {
+    inputNumber = Number(numberValue.value);
+    sign = "\u00d7";
+
+    //Conditions to generate table
+    if (numberValue.value === "" && multiplierRange.value === "") {
         alert('Fill in the fields to generate table.');
         return false;
-    } else if (numberValue == 0 && multiplierRange == 0) {
+    }
+
+    else if (numberValue.value == 0 && multiplierRange.value == 0) {
         alert('Input any number asides 0 to generate table.');
         return false;
-    } else if (multiplierRange <= 0) {
+    }
+
+    else if (multiplierRange.value <= 0) {
         alert('Input numbers above 0 in the range field.');
         return false;
-    } else {
-        for (i = 1; i <= Number(multiplierRange); i++) {
+    }
+
+    else {
+        //Loop through multiplier range for generation of table rows
+        for (i = 1; i <= inputNumber; i++) {
             result = inputNumber * i;
             tableRow = document.createElement('tr');
             tableRow.innerHTML = `<td class="values" style="border: 2px solid orange;">${inputNumber}</td>
@@ -27,5 +36,17 @@ function multiply() {
                                   <td class="output" style="border: 2px solid blue;">${result}</td>`;
             document.querySelector('tbody').appendChild(tableRow);
         }
+
+        //Disable button
+        generateBtn.disabled = true;
     }
 }
+
+function enableBtn() {
+    generateBtn.disabled = false;
+}
+
+multiplierRange.addEventListener('keydown', enableBtn);
+numberValue.addEventListener('keydown', enableBtn);
+
+
